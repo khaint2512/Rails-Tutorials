b8062e1e8c61798d6e192d66b56cfdd8d2074812
khaint2512 <trongkhai2598@gmail.com>
2019-07-29 23:01:28 +0700

    Updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def full_title page_title = ""
     base_title = t "layouts.application.base_title"
     if page_title.empty?
-        base_title
+      base_title
     else
-        page_title + " | " + base_title
+      page_title + " | " + base_title
     end
   end
 end
