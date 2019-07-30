8d86e3d6f5d932823bba23ee2366195725836fa4
khaint2512 <trongkhai2598@gmail.com>
2019-07-30 10:04:10 +0700

    Updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: Settings.user.password.min_length }
   VALID_EMAIL_REGEX = Settings.user.email.regex_valid
   validates :email, presence: true, length: { maximum: Settings.user.email.max_length }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  
+
   has_secure_password
 end
