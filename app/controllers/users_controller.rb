4a4872d410b5c151a577cbb127031a0fd306506a
khaint2512 <trongkhai2598@gmail.com>
2019-07-30 09:24:38 +0700

    Updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,27 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by id: params[:id]
+    return @user if @user
+    render file: "public/404.html", status: user_cannot_found
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".flash.success"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit :name, :email, :password, :password_confirmation
+    end
 end
