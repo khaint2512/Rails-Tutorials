15ff4fd354385c318dbc9104d200a96f41c702d6
khaint2512 <trongkhai2598@gmail.com>
2019-08-01 13:08:42 +0700

    Push

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root "static_pages#home"
 
   get "static_pages/home", to: "static_pages#home"
@@ -6,6 +8,9 @@ Rails.application.routes.draw do
   get "static_pages/about", to: "static_pages#about"
   get "static_pages/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   resources :users
 end
